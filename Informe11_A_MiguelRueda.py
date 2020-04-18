91a5d506e818be758e958e0de98639fc0ebc35b2
1MiguelSantiagoR1 <62843308+1MiguelSantiagoR1@users.noreply.github.com>
2020-04-18 14:57:59 -0500

    Preiforme 11 parte 1

---
+++ Informe11_A_MiguelRueda.py
@@ -0,0 +1,20 @@
+Punto a
+import numpy as np
+def generador(min,max):
+   x = np.random.randint(min,max , size = 48).reshape(4,12)
+   return x
+gen = generador(1,17)
+print(gen)
+
+Punto b
+import numpy as np
+def generador(min,max):
+   x = np.random.randint(min,max , size = 48).reshape(4,12)
+   return x
+ingresos = generador(100,181)
+egresos = generador(60,131)
+print("Los ingresos son: ")
+print(ingresos)
+print("")
+print("Los Egresos son: ")
+print(egresos)
--- informe11_B_MiguelRueda.py
+++
@@ -1,8 +0,0 @@
-Punto a
-import numpy as np
-def generador(min,max):
-   x = np.random.randint(min,max , size = 48).reshape(4,12)
-   return x
-gen = generador(1,17)
-
-print(gen)
